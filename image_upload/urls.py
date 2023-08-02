3edfa8210e8573f3badd618ab9692ec97a2492a6
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-08-02 16:08:48 +0800

    feat: add login page

--- image_upload/urls.py
+++ image_upload/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", views.UploadImageView.as_view(), name="image-upload"),
     path("image/<uuid>/", views.DetailUploadImageView.as_view(), name="image-detail"),
     path("image/<uuid>/delete/", views.DeleteUploadImageView.as_view(), name="image-delete"),
